4902e73c43aeedf47f42d16bb29d5bcfa64e426e
abhi64-sudo <128058726+abhi64-sudo@users.noreply.github.com>
2024-09-29 15:16:22 +0530

    Revert "Co-authored-by: abhi yadav <abhay80212@gmail.com>"
    This reverts commit 446d85fcf55e53e52af1d191e1492e0ae6f4dd21.

---
+++ swap.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a , b  , t;
+    cout<<" enter the number";
+    cin>>a;
+    cin>>b;
+    t = a;
+    a = b;
+    b= t;
+    cout<<a<<b<<endl;
+
+}
